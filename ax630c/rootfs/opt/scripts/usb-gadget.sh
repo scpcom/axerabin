cca3dcf10292638e952c0120c2a9728b0edff2ab
916BGAI <zhangjian@sipeed.com>
2026-01-20 10:01:50 +0800

    allow older Windows to auto-load NCM and RNDIS drivers

--- ax630c/rootfs/opt/scripts/usb-gadget.sh
+++ ax630c/rootfs/opt/scripts/usb-gadget.sh
@@ -40,7 +40,13 @@ usb_ncm_stop()
     #     exit 1
     # fi
 
+    ipv4_prefix=$(get_ncm_ipv4_prefix)
+    ip addr del $ipv4_prefix.1/24 dev $ncm_ifname || true
     ifconfig $ncm_ifname down
+    if [ -f /var/run/udhcpd.${ncm_ifname}.pid ]
+    then
+        kill -15 $(cat /var/run/udhcpd.${ncm_ifname}.pid)
+    fi
 
     # if [ -e configs/c.1/ncm.${ncm_ifname} ]; then
     #     rm configs/c.1/ncm.${ncm_ifname}
@@ -49,6 +55,27 @@ usb_ncm_stop()
     # if [ -d functions/ncm.${ncm_ifname} ]; then
     #     rmdir functions/ncm.${ncm_ifname}
     # fi
+}
+
+get_ncm_ipv4_prefix()
+{
+    id2=$(printf "%d" 0x$(sha512sum /proc/ax_proc/uid | head -c 2))
+    id3=$(printf "%d" 0x$(sha512sum /proc/ax_proc/uid | head -c 4 | tail -c 2))
+    id3=$((id3 + 1))
+    if [ "$id2" = "$id3" ]
+    then
+        id2=$((id2 + 1))
+    fi
+    if [ "$id2" -ge 255 ]
+    then
+        id2=253
+    fi
+    if [ "$id3" -ge 255 ]
+    then
+        id3=254
+    fi
+
+    echo 10.$id2.$id3
 }
 
 usb_ncm_start()
@@ -74,23 +101,7 @@ usb_ncm_start()
     ifconfig $ncm_ifname up
     echo "ncm mac address: ${new_ncmmac}"
 
-    id2=$(printf "%d" 0x$(sha512sum /proc/ax_proc/uid | head -c 2))
-    id3=$(printf "%d" 0x$(sha512sum /proc/ax_proc/uid | head -c 4 | tail -c 2))
-    id3=$((id3 + 1))
-    if [ "$id2" = "$id3" ]
-    then
-        id2=$((id2 + 1))
-    fi
-    if [ "$id2" -ge 255 ]
-    then
-        id2=253
-    fi
-    if [ "$id3" -ge 255 ]
-    then
-        id3=254
-    fi
-    ipv4_prefix=10.$id2.$id3
-
+    ipv4_prefix=$(get_ncm_ipv4_prefix)
     gen_udhcpd_conf $ncm_ifname "${ipv4_prefix}"  > /etc/udhcpd.${ncm_ifname}.conf
     ip addr add $ipv4_prefix.1/24 dev $ncm_ifname
     udhcpd -S /etc/udhcpd.${ncm_ifname}.conf
@@ -109,7 +120,14 @@ usb_rndis_stop()
     #     exit 1
     # fi
 
+    ipv4_prefix=$(get_rndis_ipv4_prefix)
+    ip addr del $ipv4_prefix.1/24 dev $rndis_ifname || true
+
     ifconfig $rndis_ifname down
+    if [ -f /var/run/udhcpd.${rndis_ifname}.pid ]
+    then
+        kill -15 $(cat /var/run/udhcpd.${rndis_ifname}.pid)
+    fi
 
     # if [ -e configs/c.1/rndis.${rndis_ifname} ]; then
     #     rm configs/c.1/rndis.${rndis_ifname}
@@ -118,6 +136,26 @@ usb_rndis_stop()
     # if [ -d functions/rndis.${rndis_ifname} ]; then
     #     rmdir functions/rndis.${rndis_ifname}
     # fi
+}
+
+get_rndis_ipv4_prefix()
+{
+    id2=$(printf "%d" 0x$(sha512sum /proc/ax_proc/uid | head -c 2))
+    id3=$(printf "%d" 0x$(sha512sum /proc/ax_proc/uid | head -c 4 | tail -c 2))
+    if [ "$id2" = "$id3" ]
+    then
+        id2=$((id2 + 2))
+    fi
+    if [ "$id2" -ge 255 ]
+    then
+        id2=252
+    fi
+    if [ "$id3" -ge 255 ]
+    then
+        id3=251
+    fi
+
+    echo 10.$id2.$id3
 }
 
 usb_rndis_start()
@@ -135,21 +173,7 @@ usb_rndis_start()
     ifconfig $rndis_ifname up
     echo "rndis mac address: ${new_rndismac}"
 
-    id2=$(printf "%d" 0x$(sha512sum /proc/ax_proc/uid | head -c 2))
-    id3=$(printf "%d" 0x$(sha512sum /proc/ax_proc/uid | head -c 4 | tail -c 2))
-    if [ "$id2" = "$id3" ]
-    then
-        id2=$((id2 + 2))
-    fi
-    if [ "$id2" -ge 255 ]
-    then
-        id2=252
-    fi
-    if [ "$id3" -ge 255 ]
-    then
-        id3=251
-    fi
-    ipv4_prefix=10.$id2.$id3
+    ipv4_prefix=$(get_rndis_ipv4_prefix)
 
     gen_udhcpd_conf $rndis_ifname "${ipv4_prefix}"  > /etc/udhcpd.${rndis_ifname}.conf
     ip addr add $ipv4_prefix.1/24 dev $rndis_ifname
@@ -159,7 +183,7 @@ usb_rndis_start()
 usb_gadget_stop()
 {
     usb_rndis_stop
-    # usb_ncm_stop
+    usb_ncm_stop
 
     echo > ${UDC}
     find $CONFIGFS_HOME/usb_gadget/${usb_dev_controller} -type l | xargs -I {} rm {}
@@ -167,6 +191,7 @@ usb_gadget_stop()
 
 usb_gadget_start()
 {
+    echo device > /sys/class/usb_role/8000000.dwc3-role-switch/role
     mkdir -p $CONFIGFS_HOME
     if [ ! -d "${CONFIGFS_HOME}/usb_gadget/" ]; then
             mount none $CONFIGFS_HOME -t configfs
@@ -191,13 +216,27 @@ usb_gadget_start()
     # RNDIS
     if [[ "${maix_usb_rndis}x" == "1x" ]]; then
         mkdir -p functions/rndis.${rndis_ifname}
+        rm -f configs/c.1/rndis.${rndis_ifname}
+        echo RNDIS > functions/rndis.${rndis_ifname}/os_desc/interface.rndis/compatible_id
+        echo 5162001 > functions/rndis.${rndis_ifname}/os_desc/interface.rndis/sub_compatible_id
         ln -s functions/rndis.${rndis_ifname} configs/c.1
     fi
 
     # NCM
     if [[ "${maix_usb_ncm}x" == "1x" ]]; then
         mkdir -p functions/ncm.${ncm_ifname}
+        rm -f configs/c.1/ncm.${ncm_ifname}
+        echo WINNCM > functions/ncm.${ncm_ifname}/os_desc/interface.ncm/compatible_id
         ln -s functions/ncm.${ncm_ifname} configs/c.1
+    fi
+
+    # Microsoft OS 2.0 Descriptor
+    if [[ "${maix_usb_ncm}x" == "1x" || "${maix_usb_rndis}x" == "1x" ]]; then
+        echo 1 > os_desc/use
+        echo 0xCD > os_desc/b_vendor_code
+        echo MSFT100 > os_desc/qw_sign
+        rm -f os_desc/c.1
+        ln -s configs/c.1 os_desc
     fi
 
     # mass storage
@@ -205,11 +244,45 @@ usb_gadget_start()
         mkdir -p functions/mass_storage.disk0
         mkdir -p functions/mass_storage.disk0/lun.0
         mkdir -p functions/mass_storage.disk0/lun.1
+        rm -f configs/c.1/mass_storage.disk0
         ln -s functions/mass_storage.disk0 configs/c.1/
         echo 1 > functions/mass_storage.disk0/lun.0/removable
         echo 1 > functions/mass_storage.disk0/lun.1/removable
 	    echo /dev/mmcblk0p16 > functions/mass_storage.disk0/lun.0/file # boot
         echo /dev/mmcblk0p17 > functions/mass_storage.disk0/lun.1/file # root
+    fi
+
+    if [[ "${maix_usb_hid_keyboard}x" == "1x" ]]; then
+        mkdir -p functions/hid.GS0
+        # echo 1 > functions/hid.GS0/subclass
+        # echo 1 > functions/hid.GS0/wakeup_on_write
+        echo 1 > functions/hid.GS0/protocol
+        echo 6 > functions/hid.GS0/report_length
+        echo -ne \\x05\\x01\\x09\\x06\\xa1\\x01\\x05\\x07\\x19\\xe0\\x29\\xe7\\x15\\x00\\x25\\x01\\x75\\x01\\x95\\x08\\x81\\x02\\x95\\x01\\x75\\x08\\x81\\x03\\x95\\x05\\x75\\x01\\x05\\x08\\x19\\x01\\x29\\x05\\x91\\x02\\x95\\x01\\x75\\x03\\x91\\x03\\x95\\x06\\x75\\x08\\x15\\x00\\x25\\x65\\x05\\x07\\x19\\x00\\x29\\x65\\x81\\x00\\xc0 > functions/hid.GS0/report_desc
+        rm -f configs/c.1/hid.GS0
+        ln -s functions/hid.GS0 configs/c.1
+    fi
+
+    if [[ "${maix_usb_hid_mouse}x" == "1x" ]]; then
+        mkdir -p functions/hid.GS1
+        # echo 1 > functions/hid.GS1/subclass
+        # echo 1 > functions/hid.GS1/wakeup_on_write
+        echo 2 > functions/hid.GS1/protocol
+        echo 4 > functions/hid.GS1/report_length
+        echo -ne \\x5\\x1\\x9\\x2\\xa1\\x1\\x9\\x1\\xa1\\x0\\x5\\x9\\x19\\x1\\x29\\x3\\x15\\x0\\x25\\x1\\x95\\x3\\x75\\x1\\x81\\x2\\x95\\x1\\x75\\x5\\x81\\x3\\x5\\x1\\x9\\x30\\x9\\x31\\x9\\x38\\x15\\x81\\x25\\x7f\\x75\\x8\\x95\\x3\\x81\\x6\\xc0\\xc0 > functions/hid.GS1/report_desc
+        rm -f configs/c.1/hid.GS1
+        ln -s functions/hid.GS1 configs/c.1
+    fi
+
+    if [[ "${maix_usb_hid_touchpad}x" == "1x" ]]; then
+        mkdir -p functions/hid.GS2
+        # echo 1 > functions/hid.GS2/subclass
+        # echo 1 > functions/hid.GS2/wakeup_on_write
+        echo 2 > functions/hid.GS2/protocol
+        echo 6 > functions/hid.GS2/report_length
+        echo -ne \\x05\\x01\\x09\\x02\\xa1\\x01\\x09\\x01\\xa1\\x00\\x05\\x09\\x19\\x01\\x29\\x03\\x15\\x00\\x25\\x01\\x95\\x03\\x75\\x01\\x81\\x02\\x95\\x01\\x75\\x05\\x81\\x01\\x05\\x01\\x09\\x30\\x09\\x31\\x15\\x00\\x26\\xff\\x7f\\x35\\x00\\x46\\xff\\x7f\\x75\\x10\\x95\\x02\\x81\\x02\\x05\\x01\\x09\\x38\\x15\\x81\\x25\\x7f\\x35\\x00\\x45\\x00\\x75\\x08\\x95\\x01\\x81\\x06\\xc0\\xc0 > functions/hid.GS2/report_desc
+        rm -f configs/c.1/hid.GS2
+        ln -s functions/hid.GS2 configs/c.1
     fi
 
     echo "${maix_usb_ncm} -- ${maix_usb_rndis} -- ${maix_usb_mass_storage}"
@@ -259,6 +332,10 @@ case "$1" in
 
         echo "usb gadget stop"
         usb_gadget_stop
+
+        if [ -e /var/run/udhcpd.usb0.pid ]; then
+            kill -15 $(cat /var/run/udhcpd.usb0.pid)
+        fi
         ;;
     rndis_start)
         usb_rndis_start
